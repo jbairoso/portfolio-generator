f6ebeb93f60feb0d4e1966f83f9e53100508856c
jbairoso <jbairoso@gmail.com>
2022-05-16 21:12:04 -0700

    generate entire site

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
 const inquirer = require("inquirer");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 const generatePage = require("./src/page-template");
 
 const promptUser = () => {
@@ -46,7 +46,7 @@ const promptUser = () => {
   ]);
 };
 
-const promptProject = portfolioData => {
+const promptProject = (portfolioData) => {
   console.log(`
   =================
   Add a New Project
@@ -135,17 +135,21 @@ const promptProject = portfolioData => {
       }
     });
 };
-
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile("./index.html", pageHTML, (err) => {
-      if (err) throw err;
-
-      console.log(
-        "Portfolio complete! Check out index.html to see the output!"
-      );
-    });
+    return generatePage(portfolioData);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
   });
